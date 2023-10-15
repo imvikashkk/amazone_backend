831fecbbb69d1cf78f5a4b204285630a9c60bceb
Vikash Kumar <imvikashkk@gmail.com>
2023-10-15 18:36:54 +0530

    Rainforest API change

--- src/controllers/RainforestAPI_Data.js
+++ src/controllers/RainforestAPI_Data.js
@@ -2,6 +2,13 @@ const RainforestAPI_Data = (req, res) => {
 
   const {input, filter, currentPage} = req.params
 
+  if(!input || !filter || !currentPage){
+    res.json({
+        sucess:false,
+        message:"required parameter not found !"
+    })
+  }
+
   // set up the request parameters
   const params = {
     api_key: "7D4C46625211439F88B3C0C247F23B44",
